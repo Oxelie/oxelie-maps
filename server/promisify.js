33792721dca7c8463d854ad8c89a83b1b2be87f8
Lionel Tardy <net@lionel.me>
2017-06-16 19:21:33 +0200

    Update packages for year 2017!

--- server/promisify.js
+++ server/promisify.js
@@ -1,8 +1,6 @@
 var fs = require('fs'),
     http = require('http'),
-    NeDB = require('nedb'),
     p = require('bluebird');
 
 p.promisifyAll(fs);
 p.promisifyAll(http.Server.prototype);
-p.promisifyAll(NeDB.prototype);
